294fa1dfb8a5163210291933a79ece94c66ea10f
CristianLuna77 <79183942+CristianLuna77@users.noreply.github.com>
2021-02-18 11:04:37 -0600

    Add files via upload

---
+++ tarea1/tarea1/Program.cs
@@ -0,0 +1,12 @@
+﻿bool boleano = true;
+int a = 5, b = 2;
+double c = a * b;
+string nombre = "Luis Garcia";
+
+Console.WriteLine(boleano.ToString());
+Console.WriteLine(a.ToString());
+Console.WriteLine(c.ToString());
+Console.WriteLine(nombre.ToString());
+Console.ReadKey();
+    }
+
