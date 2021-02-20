442b0998bc745926503979ce877c587fecc75eae
Cristian Luna (TheC77L) <79183942+CristianLuna77@users.noreply.github.com>
2021-02-19 19:35:08 -0600

    Add files via upload

---
+++ Ejercicio 2/Ejercicio 2/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Ejercicio_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double descuento, compra,total;
+            Console.WriteLine("****Bienvenido a MegaPlaza****");
+            Console.Write("Ingrese el Total de la compra para un descuento: ");
+            compra = Convert.ToInt32(Console.ReadLine());
+            descuento = compra * 0.20;
+            total = compra - descuento;
+            if (compra > 299)
+            {
+                
+                Console.WriteLine("Su descuento es de: " + descuento);
+                Console.WriteLine("El total a pagar es de: " + total);
+
+            }
+            else
+            {
+                Console.WriteLine("Usted no tiene ningun descuento porque su compra es menor a 300");
+                
+            
+                    }
+             
+             
+            
+        }
+    }
+}
