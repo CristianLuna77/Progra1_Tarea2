c0b98b7b98faffcbe044fa2219ffe9b93306f099
Cristian Luna (TheC77L) <79183942+CristianLuna77@users.noreply.github.com>
2021-02-19 19:26:06 -0600

    Add files via upload

---
+++ Ejercicio 1/Ejercicio 1/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Ejercicio_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            char Letra;
+            Console.Write("Introdusca una Letra: ");
+            Letra = Convert.ToChar(Console.ReadLine());
+            if (char.IsUpper(Letra))
+            {
+                Console.WriteLine("Es una letra Mayuscula");
+
+
+            }
+            else
+            {
+                Console.WriteLine("Es una letra minuscula");
+            }
+
+
+
+
+        }
+    }
+}
