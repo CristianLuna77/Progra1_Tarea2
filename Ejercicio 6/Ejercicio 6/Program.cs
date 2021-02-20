b04abdd4e26ecdc0ac7a658a2f5b29aa127bb32e
Cristian Luna (TheC77L) <79183942+CristianLuna77@users.noreply.github.com>
2021-02-19 19:39:32 -0600

    Add files via upload

---
+++ Ejercicio 6/Ejercicio 6/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Ejercicio_6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num,i, num1=0;
+            Console.Write("Digite un numero para saber si es numero primo o no: ");
+            num = int.Parse(Console.ReadLine());
+            for(i=1;i<=num;i++)
+            {
+                if (num % i == 0) 
+                {
+                    num1++;
+                }
+            }
+            if (num1>2)
+            {
+                Console.WriteLine("El numero ingresado No es primo");
+            }
+            else
+            {
+                Console.WriteLine("El numero ingresado es primo");
+            }
+
+
+
+        }
+    }
+}
