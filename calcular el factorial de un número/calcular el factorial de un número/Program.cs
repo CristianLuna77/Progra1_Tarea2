a1156cbb2dd199f8327c72f4e0a5052d60babd84
Cristian Luna (TheC77L) <79183942+CristianLuna77@users.noreply.github.com>
2021-02-26 18:40:35 -0600

    Add files via upload

---
+++ calcular el factorial de un número/calcular el factorial de un número/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace calcular_el_factorial_de_un_número
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num;
+            long fact=1;
+            Console.Write("Ingrese un numero: ");
+            num = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine(num + "! = 1");
+            for (int i = 2; i<=num; i++)
+            {
+                fact *= i;
+                Console.WriteLine(" factor:  " + i);
+            }
+            Console.WriteLine("\t= " + fact);
+        }
+    }
+}
---
+++ calcular la serie de un numero/calcular la serie de un numero/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace calcular_la_serie_de_un_numero
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num, x = 0, y = 1, z = 1;
+            Console.Write("Ingrese el numero de elementos: ");
+            num = Convert.ToInt32(Console.ReadLine());
+            for (int i = 1; i < num; i++)
+            {
+                z = x + y;
+                Console.WriteLine(z + " ");
+                x = y;
+                y = z;
+            }
+        }
+    }
+}
