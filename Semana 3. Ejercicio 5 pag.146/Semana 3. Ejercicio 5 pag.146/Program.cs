77f6bff8482ce7bca76c0aebf9c6c37e5e26b56e
Cristian Luna (TheC77L) <79183942+CristianLuna77@users.noreply.github.com>
2021-02-26 18:39:55 -0600

    Add files via upload

---
+++ Semana 3. Ejercicio 5 pag.146/Semana 3. Ejercicio 5 pag.146/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace Semana_3._Ejercicio_5_pag._146
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num,i,mayor=0,menor=0;
+            Console.Write("Cuantas Edades desea ingresar: ");
+            num = Convert.ToInt32(Console.ReadLine());
+            int[] year= new int[num + 1];
+            for (i = 1; i <= num; i++)
+            {
+                Console.Write("Cual es la Edad de la Persona: ");
+                year[i]= Convert.ToInt32(Console.ReadLine());
+            }
+            mayor = year[1];
+            menor = year[1];
+            for (i = 1; i <= num; i++)
+            {
+                if (year[i] > mayor)
+                {
+                    mayor = year[i];
+                }
+                else if (year[i] < menor)
+                {
+                    menor =year[i];
+                }
+            }
+            
+            Console.WriteLine("La edad mayor es: " + mayor);
+            Console.WriteLine("La edad menor es: " + menor);
+        }
+        }
+    }
+
