518c811fe2c5d009f644cb4ca9ca7f6bd7e8bdda
Cristian Luna (TheC77L) <79183942+CristianLuna77@users.noreply.github.com>
2021-02-26 18:36:49 -0600

    Add files via upload

---
+++ Semana 3. Ejercicio 4 pag.110/Semana 3. Ejercicio 4 pag.110/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+
+namespace Semana_3._Ejercicio_4_pag._110
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num;
+            Aqui:
+            Console.Write("Ingrese un numero del 1 al 7: ");
+            num = int.Parse(Console.ReadLine());
+
+            switch (num)
+            {
+                case 1:
+                    {
+                        Console.WriteLine("Lunes");
+                        break;
+                    }
+                case 2:
+                    {
+                        Console.WriteLine("Martes");
+                        break;
+                    }
+                case 3:
+                    {
+                        Console.WriteLine("Miercoles");
+                        break;
+                    }
+                case 4:
+                    {
+                        Console.WriteLine("Jueves");
+                        break;
+                    }
+                case 5:
+                    {
+                        Console.WriteLine("Viernes");
+                        break;
+                    }
+                case 6:
+                    {
+                        Console.WriteLine("Sabado");
+                        break;
+                    }
+                case 7:
+                    {
+                        Console.WriteLine("Domingo");
+                        break;
+                    }
+                default:
+                    {
+                        Console.WriteLine("A no mi hermano, ingrese el numero del rango que le doy");
+                        goto Aqui;
+                    }
+
+            }
+            
+        }
+    }
+}
