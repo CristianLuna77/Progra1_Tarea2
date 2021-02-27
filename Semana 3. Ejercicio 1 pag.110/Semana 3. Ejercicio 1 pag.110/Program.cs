2ed8389c55d2aa3e18d55ed31e68104ff4879da8
Cristian Luna (TheC77L) <79183942+CristianLuna77@users.noreply.github.com>
2021-02-26 18:35:25 -0600

    Add files via upload

---
+++ Semana 3. Ejercicio 1 pag.110/Semana 3. Ejercicio 1 pag.110/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Ejercicio_6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num1 = 0;
+            Console.Write("Digite un numero para saber si es numero primo o impar: ");
+            num1 = int.Parse(Console.ReadLine());
+                if (num1 % 2 == 0)
+                {
+                    Console.WriteLine("El numero ingresado es par");
+                }
+                else
+                {
+                    Console.WriteLine("El numero ingresado es impar");
+                }
+
+
+
+         }
+    }
+}
+
