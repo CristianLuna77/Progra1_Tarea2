30a1f6655ec22a8daea031504c028194b4c39073
Cristian Luna (TheC77L) <79183942+CristianLuna77@users.noreply.github.com>
2021-02-26 18:38:38 -0600

    Add files via upload

---
+++ Semana 3. Ejercicio 1 pag.146/Semana 3. Ejercicio 1 pag.146/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace Semana_3._Ejercicio_1_pag._146
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num,i,j;
+            Console.WriteLine("\a**Tabalas disponibles del 1 al 10**");
+            Console.Write("Digite el numero de tabla que desea imprimir: ");
+            num = Convert.ToInt32(Console.ReadLine());
+            while ((num <= 0) || (num >= 11))
+            {
+                Console.Clear();
+                Console.WriteLine("Numero de rango: " + num);
+                Console.Write("El numero " + num + " no es valido! Digite un numero para una tabla de multiplicar: ");
+                num = int.Parse(Console.ReadLine());
+            }
+            Console.Clear();
+            for (i = num; i <= num; i++)
+            {
+                Console.WriteLine("\tTabla del " + i + "\n");
+
+                for (j = 1; j <= 10; j++)
+                {
+                    Console.WriteLine(+i + " * " + j + " = " + j * i);
+
+                }
+                Console.WriteLine("");
+            }
+        }
+
+    }
+}
