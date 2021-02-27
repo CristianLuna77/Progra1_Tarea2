eb644a02b81d51128efdf4931895f7adc3c46796
Cristian Luna (TheC77L) <79183942+CristianLuna77@users.noreply.github.com>
2021-02-26 18:39:01 -0600

    Add files via upload

---
+++ Semana 3. Ejercicio 2 pag.146/Semana 3. Ejercicio 2 pag.146/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Semana_3._Ejercicio_2_pag._146
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num, potencia, resultado;
+            Console.Write("Digite un numero: ");
+            num = int.Parse(Console.ReadLine());
+            Console.Write("Digite la potencia que desea elevar: ");
+            potencia = int.Parse(Console.ReadLine());
+            resultado = (int) Math.Pow(num, potencia);
+            Console.WriteLine("El resultado es: " + resultado);
+        }
+    }
+}
