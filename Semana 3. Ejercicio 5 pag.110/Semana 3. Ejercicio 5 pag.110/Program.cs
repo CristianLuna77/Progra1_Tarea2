f35f276f685ffa2aa7ef5d0e7358644dd516054c
Cristian Luna (TheC77L) <79183942+CristianLuna77@users.noreply.github.com>
2021-02-26 18:38:11 -0600

    Add files via upload

---
+++ Semana 3. Ejercicio 5 pag.110/Semana 3. Ejercicio 5 pag.110/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace Semana_3._Ejercicio_5_pag._110
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num;
+            float resultado, num2, longi, apotema;
+            Console.WriteLine("Que desea hacer");
+            Console.WriteLine("1 - calcular el perimetro");
+            Console.WriteLine("2 - calcular el area de cualquier poligono regular");
+            Console.Write("............       ");
+            num = int.Parse(Console.ReadLine());
+            if (num == 1)
+            {
+                Console.Write("Introduzca el diametro: ");
+                num2 = float.Parse(Console.ReadLine());
+                resultado = (float)(num2 * 3.14159);
+                Console.WriteLine("El resultado es de: " + resultado);
+            }
+            else if (num == 2)
+            {
+                Console.Write("Introduzca la longitud: ");
+                longi = float.Parse(Console.ReadLine());
+                Console.Write("Introduzca la longitud: ");
+                apotema = float.Parse(Console.ReadLine());
+                resultado = (float)(5 * longi * apotema);
+                Console.WriteLine("El resultado es de: " + resultado);
+
+
+            }
+            
+        }
+    }
+}
